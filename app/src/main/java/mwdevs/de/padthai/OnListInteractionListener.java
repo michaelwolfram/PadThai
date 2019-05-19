d9d2ab018aa61f5d2abcb2d0977c3230fe371ce1
Michael Wolfram <michaelwolfram@gmx.de>
2019-05-19 16:10:53 +0200

    refactor interface

--- app/src/main/java/mwdevs/de/padthai/OnListInteractionListener.java
+++ app/src/main/java/mwdevs/de/padthai/OnListInteractionListener.java
@@ -2,5 +2,6 @@ package mwdevs.de.padthai;
 
 public interface OnListInteractionListener {
     void onListItemClick(ShoppingListContent.ShoppingItem item);
+
     void onListItemLongClick(ShoppingListContent.ShoppingItem item);
 }
