08a0f57f50e038b13d3f6d1fd53c3db1f242ab34
Michael Wolfram <michaelwolfram@gmx.de>
2019-07-07 16:02:42 +0200

    update/finalize DishInfo to include all dishes with their components

--- app/src/main/java/de/mwdevs/padthai/main/data/DishInfo.java
+++ app/src/main/java/de/mwdevs/padthai/main/data/DishInfo.java
@@ -8,7 +8,6 @@ import de.mwdevs.padthai.R;
 
 public enum DishInfo {
 
-    // TODO: 23.06.19 put these also in json file
     PAD_THAI(0, R.string.pad_thai, R.drawable.pad_thai_512, 0,
             new ArrayList<>(Arrays.asList(
                     new DishComponentInfo(R.string.chili_paste, "chili_paste.json"),
@@ -17,36 +16,33 @@ public enum DishInfo {
             ))),
     GREEN_PAPAYA_SALAD(1, R.string.green_papaya_salad, R.drawable.green_papaya_salad_512, 3,
             new ArrayList<>(Collections.singletonList(
-                    new DishComponentInfo(R.string.peanut_sauce, "green_papaya_salad.json")
+                    new DishComponentInfo(R.string.green_papaya_salad, "green_papaya_salad.json")
             ))),
     MASSAMAN_CURRY(2, R.string.massaman_curry, R.drawable.massaman_curry_512, 4,
             new ArrayList<>(Collections.singletonList(
                     new DishComponentInfo(R.string.massaman_curry, "massaman_curry.json")
             ))),
     GREEN_THAI_CURRY(3, R.string.green_thai_curry, R.drawable.green_thai_curry_512, 5,
-            new ArrayList<>(Arrays.asList(
-                    new DishComponentInfo(R.string.chili_paste, "chili_paste.json"),
-                    new DishComponentInfo(R.string.peanut_sauce, "peanut_sauce.json")
-            ))),
-    TOM_YAM_SOUP(4, R.string.tom_yam_soup, R.drawable.tom_yam_soup_512, 7,
-            new ArrayList<>(Arrays.asList(
-                    new DishComponentInfo(R.string.chili_paste, "chili_paste.json"),
-                    new DishComponentInfo(R.string.peanut_sauce, "peanut_sauce.json"),
-                    new DishComponentInfo(R.string.pad_thai, "pad_thai.json")
-            ))),
-    SPRING_ROLLS(5, R.string.spring_roles, R.drawable.spring_roles_512, 10,
-            new ArrayList<>(Arrays.asList(
-                    new DishComponentInfo(R.string.chili_paste, "chili_paste.json"),
-                    new DishComponentInfo(R.string.peanut_sauce, "peanut_sauce.json")
-            ))),
-    PUMPKIN_HUMUS(6, R.string.pumpkin_humus, R.drawable.pumpkin_humus_512, 12,
             new ArrayList<>(Collections.singletonList(
-                    new DishComponentInfo(R.string.chili_paste, "chili_paste.json")
+                    new DishComponentInfo(R.string.green_thai_curry, "green_thai_curry.json")
             ))),
-    MANGO_WITH_STICKY_RICE(7, R.string.mango_sticky_rice, R.drawable.mango_sticky_rice_512, 13,
+    TOM_YAM_SOUP(4, R.string.tom_yam_soup, R.drawable.tom_yam_soup_512, 6,
+            new ArrayList<>(Collections.singletonList(
+                    new DishComponentInfo(R.string.tom_yam_soup, "tom_yam_soup.json")
+            ))),
+    SPRING_ROLLS(5, R.string.spring_roles, R.drawable.spring_roles_512, 7,
             new ArrayList<>(Arrays.asList(
-                    new DishComponentInfo(R.string.chili_paste, "chili_paste.json"),
-                    new DishComponentInfo(R.string.peanut_sauce, "peanut_sauce.json")
+                    new DishComponentInfo(R.string.peanut_sauce, "peanut_sauce.json"),
+                    new DishComponentInfo(R.string.spring_roles, "spring_roles.json")
+            ))),
+    PUMPKIN_HUMUS(6, R.string.pumpkin_humus, R.drawable.pumpkin_humus_512, 9,
+            new ArrayList<>(Collections.singletonList(
+                    new DishComponentInfo(R.string.pumpkin_humus, "pumpkin_humus.json")
+            ))),
+    MANGO_WITH_STICKY_RICE(7, R.string.mango_with_sticky_rice, R.drawable.mango_with_sticky_rice_512, 10,
+            new ArrayList<>(Arrays.asList(
+                    new DishComponentInfo(R.string.sticky_rice, "sticky_rice.json"),
+                    new DishComponentInfo(R.string.mango_with_sticky_rice, "mango_sticky_rice.json")
             )));
 
     private int mId;
