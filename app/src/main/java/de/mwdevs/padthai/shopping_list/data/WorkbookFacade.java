c05c14d8be68b8bfc649144cddc83d64cb01e82e
Michael Wolfram <michaelwolfram@gmx.de>
2019-07-27 18:27:16 +0200

    fix/refactor ID row handling in workbook facade

--- app/src/main/java/de/mwdevs/padthai/shopping_list/data/WorkbookFacade.java
+++ app/src/main/java/de/mwdevs/padthai/shopping_list/data/WorkbookFacade.java
@@ -12,7 +12,7 @@ import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import java.util.ArrayList;
 
 public class WorkbookFacade {
-    private static final int EXCEL_SHEET_ROW_OFFSET = 7;
+    private static final int FIRST_ID_ROW_INDEX = 6;
     private static final String COLUMN_ID = "D";
     private static final String COLUMN_GRAM = "M";
     private static final String COLUMN_STK = "Q";
@@ -49,33 +49,33 @@ public class WorkbookFacade {
     public ArrayList<ShoppingListContent.ShoppingItem> createShoppingItems() {
         ArrayList<ShoppingListContent.ShoppingItem> items = new ArrayList<>();
 
-        for (int pos = 0; pos <= mSheet0.getPhysicalNumberOfRows() - EXCEL_SHEET_ROW_OFFSET; pos++) {
-            int item_id = getItemId(pos);
+        for (int row = FIRST_ID_ROW_INDEX; row <= mSheet0.getLastRowNum(); ++row) {
+            int item_id = getItemId(row);
             ShoppingListContent.ShoppingItem item = ShoppingListContent.ITEM_PROPERTY_MAP.get(item_id);
             if (item == null) {
                 Log.e(WorkbookFacade.class.getName(), "item was null. It was not added! Its ID was: " + item_id);
                 continue;
             }
-            double item_gram = getItemGram(pos);
+            double item_gram = getItemGram(row);
             if (item_gram == 0)  // Do not add the item to the shopping list if it's 0 gram.
                 continue;
-            double item_stk = getItemStk(pos);
+            double item_stk = getItemStk(row);
             item.setGramAndStk(item_gram, item_stk);
             items.add(item);
         }
         return items;
     }
 
-    private int getItemId(int position) {
-        return (int) getNumericCellValue(mSheet0, COLUMN_ID, EXCEL_SHEET_ROW_OFFSET + position);
+    private int getItemId(int row) {
+        return (int) getNumericCellValue(mSheet0, COLUMN_ID, row);
     }
 
-    private double getItemGram(int position) {
-        return getNumericCellValue(mSheet0, COLUMN_GRAM, EXCEL_SHEET_ROW_OFFSET + position);
+    private double getItemGram(int row) {
+        return getNumericCellValue(mSheet0, COLUMN_GRAM, row);
     }
 
-    private double getItemStk(int position) {
-        return getNumericCellValue(mSheet0, COLUMN_STK, EXCEL_SHEET_ROW_OFFSET + position);
+    private double getItemStk(int row) {
+        return getNumericCellValue(mSheet0, COLUMN_STK, row);
     }
 
     private void setCellValueInColumnB(@NonNull Sheet sheet, int row, double value) {
@@ -88,7 +88,7 @@ public class WorkbookFacade {
     }
 
     private double getNumericCellValue(@NonNull Sheet sheet, String column, int row) {
-        Cell cell = sheet.getRow(row - 1).getCell(CellReference.convertColStringToIndex(column));
+        Cell cell = sheet.getRow(row).getCell(CellReference.convertColStringToIndex(column));
         return cell.getNumericCellValue();
     }
 }
