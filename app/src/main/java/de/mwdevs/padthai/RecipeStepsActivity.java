613fe40e70f3e7d09ca8d70735db21db0404d3ca
Michael Wolfram <michaelwolfram@gmx.de>
2020-01-02 22:35:17 +0100

    Intercept up/down flings in RecipeStepsActivity for dish component switches

--- app/src/main/java/de/mwdevs/padthai/RecipeStepsActivity.java
+++ app/src/main/java/de/mwdevs/padthai/RecipeStepsActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
 import de.mwdevs.padthai.recipe_steps.RecipeStepsPagerAdapter;
 
-public class RecipeStepsActivity extends AppCompatActivity {
+interface OnVerticalFlingListener {
+    void onFlingUp();
 
+    void onFlingDown();
+}
+
+public class RecipeStepsActivity extends AppCompatActivity implements OnVerticalFlingListener {
     public static final String DISH_COMPONENT_QUANTITIES = "DISH_COMPONENT_QUANTITIES";
     public static final String JSON_FILENAMES = "JSON_FILENAMES";
     public static final String INITIAL_COMPONENT = "INITIAL_COMPONENT";
@@ -25,6 +33,8 @@ public class RecipeStepsActivity extends AppCompatActivity {
     private String[] mJsonFilenameArray;
     private int[] mLastActiveItemArray;
 
+    private GestureDetectorCompat mDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -34,6 +44,8 @@ public class RecipeStepsActivity extends AppCompatActivity {
 
         consumeIndent();
         setupViewPagerAndStuff();
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener(this));
     }
 
     private void setupViewPagerAndStuff() {
@@ -83,5 +95,77 @@ public class RecipeStepsActivity extends AppCompatActivity {
         current_component = intent.getIntExtra(INITIAL_COMPONENT, 0);
         num_components = mComponentQuantityArray.length;
         mLastActiveItemArray = new int[num_components];
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public void onFlingUp() {
+        onFlingUpOrDown(num_components - 1, 1);
+    }
+
+    @Override
+    public void onFlingDown() {
+        onFlingUpOrDown(0, -1);
+    }
+
+    private void onFlingUpOrDown(int reference_index, int component_delta) {
+        if (current_component == reference_index)
+            return;
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        mLastActiveItemArray[current_component] = viewPager.getCurrentItem();
+
+        current_component += component_delta;
+
+        changeDishComponent(viewPager);
+    }
+
+    private void changeDishComponent(ViewPager viewPager) {
+        recipeStepsPagerAdapter.updateData(
+                mComponentQuantityArray[current_component], mJsonFilenameArray[current_component]);
+
+        viewPager.setCurrentItem(mLastActiveItemArray[current_component]);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        OnVerticalFlingListener onVerticalFlingListener_;
+
+        MyGestureListener(OnVerticalFlingListener onVerticalFlingListener) {
+            onVerticalFlingListener_ = onVerticalFlingListener;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            float dx = event2.getX() - event1.getX();
+            float dy = event2.getY() - event1.getY();
+            float dx_abs = Math.abs(dx);
+            float dy_abs = Math.abs(dy);
+
+            boolean is_horizontal = dx_abs >= dy_abs;
+            boolean is_down = dy > 0;
+
+            boolean fling_up = !is_horizontal && !is_down;
+            boolean fling_down = !is_horizontal && is_down;
+
+            if (fling_up) {
+                onVerticalFlingListener_.onFlingUp();
+                return false;
+            } else if (fling_down) {
+                onVerticalFlingListener_.onFlingDown();
+                return false;
+            }
+            return true;
+        }
     }
 }
